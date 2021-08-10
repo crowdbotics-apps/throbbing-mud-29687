29cc92a3f48a8d8491be974397722dbfcc173c93
Crowdbotics <team@crowdbotics.com>
2021-08-10 11:57:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "throbbing_mud_29687Identifier",
+  url: "https://throbbing_mud_29687Identifier.botics.co",
+  api: "https://throbbing_mud_29687Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
